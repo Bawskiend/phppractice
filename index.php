4229c97b544a110b0efb4eb1f625e9e38d9543ab
Bawskiend <Frayzerka@gmail.com>
2022-02-21 20:35:12 +0500

    firstpc 21/02

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
-    $url = $_SERVER['REQUEST_URI'];
-	$path = 'view' . $url . '.php';
-	
-	if (file_exists($path)) {
-		$content = file_get_contents($path);
-	} else {
-		header('HTTP/1.0 404 Not Found');
-		$content = file_get_contents('view/404.php');
-	}
-	$layout  = file_get_contents('layout.php');
-	
-	$layout = str_replace('{{ content }}', $content, $layout);
-	
-	echo $layout;
+    function func($str){
+        $reg = '#[0-9]#';
+        echo preg_match_all($reg,$str);
+    }
+    func('1111');
 ?> 
 <meta charset="UTF-8">
