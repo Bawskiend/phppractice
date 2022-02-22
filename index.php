b2a64aa6b9fd2a7f0b57a1a58ef855b6aed3ba60
Румянцев Артём Игоревич <A.Rumyantsev@UKZ.LOCAL>
2022-02-22 13:59:31 +0500

    22.02 workpc

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<meta charset="UTF-8">
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/style.css">
 <?php
     $url = $_SERVER['REQUEST_URI'];
-	$url = preg_replace('#/#','\\',$url);
-	echo $url;
+    if(file_exists($url)){
+	$url = preg_replace('#/#','',$url);
     $layout = file_get_contents('layout.php');
     $content = file_get_contents('.' . $url);
     $layout = str_replace('{{ content }}', $content, $layout);
-    
-    echo $layout;
+    echo $layout;}
+    elseif(!file_exists($url)){
+        $layout = file_get_contents('view/404.php');
+        $content = file_get_contents('index.php');
+        $layout = str_replace('{{ content }}', $content, $layout);
+        echo $layout;
+    }
 ?> 
-<meta charset="UTF-8">
-<!DOCTYPE html>
