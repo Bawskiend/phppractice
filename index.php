42ce033b9af313a0c7cbb8af44e10e07156ddc55
Румянцев Артём Игоревич <A.Rumyantsev@UKZ.LOCAL>
2022-02-22 17:49:53 +0500

    22.02 workpc

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
-<meta charset="UTF-8">
-<!DOCTYPE html>
-<link rel="stylesheet" href="css/style.css">
 <?php
     $url = $_SERVER['REQUEST_URI'];
     if(file_exists($url)){
-	$url = preg_replace('#/#','',$url);
-    $layout = file_get_contents('layout.php');
-    $content = file_get_contents('.' . $url);
-    $layout = str_replace('{{ content }}', $content, $layout);
-    echo $layout;}
-    elseif(!file_exists($url)){
-        $layout = file_get_contents('view/404.php');
-        $content = file_get_contents('index.php');
+        $url = preg_replace('#/#','',$url);
+        $layout = file_get_contents('layout.php');
+        $content = file_get_contents('.' . $url);
         $layout = str_replace('{{ content }}', $content, $layout);
-        echo $layout;
-    }
-?> 
+        echo $layout;}
+        elseif(!file_exists($url)){
+            $layout = file_get_contents('view/404.php');
+            $content = file_get_contents('index.php');
+            $layout = str_replace('{{ content }}', $content, $layout);
+            echo $layout;
+        }
+        ?> 
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="css/style.css">
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
