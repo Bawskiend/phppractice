b844d51d882aa9d366250ede617278301c09ca53
Румянцев Артём Игоревич <A.Rumyantsev@UKZ.LOCAL>
2022-05-11 09:01:40 +0500

    11.05 workpc

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
-    $url = $_SERVER['REQUEST_URI'];
-    $path = 'php' . $url . '.php';
-    $path = str_replace('\\','./',$path);
-    if(file_exists($path)){
-        $layout = file_get_contents('php\layout.php');
-        $content = file_get_contents($path);
-        $layout = str_replace('{{ content }}', $content, $layout);    
-        echo $layout;
+    	abstract class User{
+            private $name;
+            public function SetName($name){
+                $this->name = $name;
+            }
+            public function GetName(){
+                return $this->name;
+            }
+            abstract public function increaseRevenue(){
+
+            }
         }
-    else{
-        $layout = file_get_contents('php\layout.php');
-        $content = file_get_contents('php\404.php');
-        $layout = str_replace('{{ content }}', $content, $layout);
-        echo $layout;
-    }
-        ?> 
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="css/style.css">
-            <title>Document</title>
-        </head>
-        <body>
-            
-        </body>
-        </html>
+        class Employee extends User{
+            private $salary;
+            public function SetSalary($salary){
+                $this->salary = $salary;
+            }
+            public function GetSalary(){
+                return $this->salary;
+            }
+            public function increaseRevenue($salary){
+                $this->$salary = $this->salary + $salary;
+            }
+        }
+        $employee1 = new Employee;
+        $employee1->SetName('john');
+        echo $employee1->GetName();
+?>
+
---
+++ php/practice.php
@@ -0,0 +1,16 @@
+<?php
+    class Employee{
+        public $name;
+        public $age;
+        public $salary;
+    }
+    $employee1 = new Employee;
+    $employee1->name = 'eric';
+    $employee1->age = 25;
+    $employee1->salary = 1000;
+    $employee2 = new Employee;
+    $employee2->name = 'edik';
+    $employee2->age = 26;
+    $employee2->salary = 2000;
+    var_dump($employee1->salary);
+?>
