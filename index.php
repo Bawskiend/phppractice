23baa271e23d74178c5000e064cfe47eb3ce2458
Румянцев Артём Игоревич <A.Rumyantsev@UKZ.LOCAL>
2022-05-26 09:17:56 +0500

    26.05 workpc

--- index.php
+++ index.php
@@ -1,43 +1,14 @@
 <?php
-    session_start();
-    $url = $_SERVER['REQUEST_URI'];
-    function rgp($url) { 
-        return preg_replace('/^([^?]+)(\?.*?)?(#.*)?$/', '$1$3', $url);
-    }
-    $url = rgp($url);
-    if($url == '' or $url == '/'){
-        header('Location: mainpage');
-    }
-    $content = 'php' . $url . '.php';
-    if(file_exists($content)){
-        $header = 'php/header.php';
-        ob_start();
-    }
-    elseif(!file_exists($content)){
-        $header = 'php/header.php';
-        $content = 'php/404.php';
-    }
-        ?>
-<html>
-    <meta charset="UTF-8">
-    <head>
-		<title>title</title>
-		<link rel="stylesheet" href="css/style.css">
-        <?php if($url == '/login'){
-            echo "<script src='/js/script.js'></script>";
-        } ?>
-	</head>
-	<body>
-		<header class="header">
-           <?php include_once $header ?>
-            </header>
-            <main class="app-container">
-			<?php include_once $content?>
-		</div>
-		</main>
-		<footer class="footer">
-        <? //include_once $footer?> 
-			
-		</footer>
-	</body>
-</html>         
+namespace Project\Data;
+	use \Project\Data\Controller\Page as ControllerPage;
+  use \Project\Data\Model\Page as ModelPage;
+class Test
+{
+  public function __construct()
+  {
+    $pageController  = new ControllerPage;
+    $pageModel       = new ModelPage;
+  }
+}
+
+?>
